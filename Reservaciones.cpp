0b046d054ae132d19ea7c67337ba089a393049d8
Javier Rivera <friverar5@miumg.edu.gt>
2025-10-15 21:57:14 -0600

    probando editar

--- Reservaciones.cpp
+++ Reservaciones.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 
 using namespace std;
-
+//probando editar
 int main() {
-	using namespace std;
+
 
     cout << "Hola mundo" << endl;
 	cout << "Adios mundo" << endl;
 	cout << "El bonito" << endl;
+	
 	return 0;
 }
